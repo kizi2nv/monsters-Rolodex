f0e83d17aed2d3139b6f086a679e278b165cc8ae
Duru Kizito <Kizito2nv@gmail.com>
2020-05-19 09:34:08 +0100

    Updates

---
+++ precache-manifest.e3abd4e00b4a84a44c5a6acee376eda9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fde93b3441729915186754e4d048b35",
+    "url": "/monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "37821f0f71c958d65265",
+    "url": "/monsters-Rolodex/static/css/main.812c56d7.chunk.css"
+  },
+  {
+    "revision": "d76556e39c2928d5a06e",
+    "url": "/monsters-Rolodex/static/js/2.0a976524.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-Rolodex/static/js/2.0a976524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37821f0f71c958d65265",
+    "url": "/monsters-Rolodex/static/js/main.51404c2b.chunk.js"
+  },
+  {
+    "revision": "d4edf82256a6074ff6b1",
+    "url": "/monsters-Rolodex/static/js/runtime-main.68aad3a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-Rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
